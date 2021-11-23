b5f82497d4704d1b5da7ce5f1da89d76a3f27fac
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-11-23 00:23:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("-Programa p/ ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.");
+            //Console.WriteLine();
+
+            //Pessoa p1 = new Pessoa();
+            //Pessoa p2 = new Pessoa();
+
+            //Console.WriteLine("Dados da primeira pessoa:");
+            //Console.Write("Nome: ");
+            //p1.Nome = Console.ReadLine();
+            //Console.Write("Idade: ");
+            //p1.Idade = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Dados da segunda pessoa:");
+            //Console.Write("Nome: ");
+            //p2.Nome = Console.ReadLine();
+            //Console.Write("Idade: ");
+            //p2.Idade = int.Parse(Console.ReadLine());
+
+            //if (p1.Idade > p2.Idade)
+            //{
+            //    Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            //}
+            
+            //Console.WriteLine();
+            //Console.WriteLine("=======================================================================================================");
+            //Console.WriteLine();
+
+            //Console.WriteLine("-Programa p/ ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.");
+            //Console.WriteLine();
+
+            //Funcionario f1 = new Funcionario();
+            //Funcionario f2 = new Funcionario();
+
+            //Console.WriteLine("Dados do primeiro funcionário:");
+            //Console.Write("Nome: ");
+            //f1.Nome = Console.ReadLine();
+            //Console.Write("Salário: ");
+            //f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine("Dados do segundo funcionário:");
+            //Console.Write("Nome: ");
+            //f2.Nome = Console.ReadLine();
+            //Console.Write("Salário: ");
+            //f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //double media = (f1.Salario + f2.Salario) / 2.0;
+
+            //Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("=======================================================================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("-Fazer um programa para ler os valores da largura e altura de um retângulo.Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.Usar uma classe como mostrado no projeto ao lado.");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Classes
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+    }
+}
