ceb2511471e6aef68c5a0d46289a0599592a33bd
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-11-30 00:29:21 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -77,32 +77,32 @@ namespace Exercicio_Classes
             //Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
             //Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine();
-            Console.WriteLine("=======================================================================================================");
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("=======================================================================================================");
+            //Console.WriteLine();
 
-            Console.WriteLine(" 4°-Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto).Em seguida, mostrar os dados do funcionário(nome e salário líquido).Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada(somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.Use a classe projetada abaixo.");
-            Console.WriteLine();
+            //Console.WriteLine(" 4°-Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto).Em seguida, mostrar os dados do funcionário(nome e salário líquido).Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada(somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.Use a classe projetada abaixo.");
+            //Console.WriteLine();
 
-            Funcionario func = new Funcionario();
+            //Funcionario func = new Funcionario();
 
-            Console.Write("Nome: ");
-            func.Nome = Console.ReadLine();
-            Console.Write("Salário bruto: ");
-            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Imposto: ");
-            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Nome: ");
+            //func.Nome = Console.ReadLine();
+            //Console.Write("Salário bruto: ");
+            //func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Imposto: ");
+            //func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine();
-            Console.WriteLine("Funcionário: " + func);
+            //Console.WriteLine();
+            //Console.WriteLine("Funcionário: " + func);
 
-            Console.WriteLine();
-            Console.WriteLine("Deseja aumentar o salário em qual porcentagem ? ");
-            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            func.AumentarSalario(porcent);
+            //Console.WriteLine();
+            //Console.WriteLine("Deseja aumentar o salário em qual porcentagem ? ");
+            //double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //func.AumentarSalario(porcent);
 
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + func);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + func);
 
             Console.WriteLine();
             Console.WriteLine("=======================================================================================================");
@@ -111,7 +111,29 @@ namespace Exercicio_Classes
             Console.WriteLine(" 5°-Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada).Ao final, mostrar qual a nota final do aluno no ano.Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado(que é 60 pontos). Você deve criar uma classe Aluno para resolver este problema.");
             Console.WriteLine();
 
+            Aluno aluno = new Aluno();
 
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = "
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                    + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS");
+            }
         }
     }
 }
